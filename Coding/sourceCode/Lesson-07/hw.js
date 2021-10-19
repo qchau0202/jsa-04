6d38e9540a716f63f8a020ae370d63a3acee3c67
Chou Lee <jasonwong@chous-device.local>
2021-10-19 12:44:18 +0700

    testing

---
+++ Coding/sourceCode/Lesson-07/hw.js
@@ -0,0 +1,32 @@
+// let inputEl = document.createElement("input");
+// inputEl.type = "text";
+// inputEl.className = "myInput text-white font-bold";
+
+// parentEl.appendChild(inputEl);
+
+const parentEl = document.getElementById("parent");
+let parentCSS = parentEl.style
+parentCSS.border = "solid";
+parentCSS.fontSize = "200px";
+parentCSS.width = "100%"
+parentCSS.height = "1000px"
+parentCSS.padding = "10px 100px 0px 100px"
+parentCSS.display = "flex"
+parentCSS.justifyContent ="center"
+parentCSS.flexWrap = "wrap"
+
+
+for (let i = 0; i < 6; i++) {
+    let product = document.createElement("div");
+    parentEl.appendChild(product);
+    product.className = "product"
+    let word = document.createElement("p")
+    product.appendChild(word);
+    let image = document.createElement("img")
+    product.appendChild(image)
+    let productCSS = product.style
+    productCSS.width = "30%"
+    productCSS.padding = "24px"
+    productCSS.height = "500px"
+    productCSS.border="solid"
+}
