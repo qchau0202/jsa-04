c2d58cfa24ab22733175e44240e30ea7631db874
quocchau0202 <quocchau4729@gmail.com>
2021-10-22 12:43:39 +0700

    Lesson-07-HW

--- Coding/sourceCode/Lesson-07/hw.js
+++ Coding/sourceCode/Lesson-07/hw.js
@@ -6,31 +6,64 @@
 
 const parentEl = document.getElementById("parent");
 let parentCSS = parentEl.style
-parentCSS.border = "solid";
+// parentCSS.border = "solid";
 parentCSS.fontSize = "200px";
 parentCSS.width = "100%"
 parentCSS.height = "1000px"
-parentCSS.padding = "10px 100px 0px 100px"
+parentCSS.padding = "0px 225px 0px 225px"
 parentCSS.display = "flex"
-parentCSS.justifyContent ="center"
+parentCSS.justifyContent ="space-between"
 parentCSS.flexWrap = "wrap"
-
-
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 7; i++) {
+    // Box
     let product = document.createElement("div");
     parentEl.appendChild(product);
     product.className = "product"
-    let word = document.createElement("p")
-    product.appendChild(word);
     let image = document.createElement("img")
     product.appendChild(image)
+    let word  = document.createElement("p")
+    product.appendChild(word);
+    let price = document.createElement("p")
+    product.appendChild(price)
     let productCSS = product.style
-    productCSS.width = "28%"
-    productCSS.padding = "24px"
-    productCSS.height = "500px"
-    productCSS.border = "solid"
+    productCSS.width = "32%"
+    // productCSS.padding = "24px"
+    productCSS.height = "450px"
+    // productCSS.border = "1px solid"
     productCSS.margin = "0px 0px 40px 0px"
-    productCSS.backgroundColor = "rgba(249,250,251,var(--tw-bg-opacity))"
+    productCSS.padding = "0px 0px 20px 0px"
+    productCSS.backgroundColor = "rgba(249,250,251)"
     productCSS.borderRadius="0.5rem"
-    productCSS.boxSizing = "border-box"
+    productCSS.boxSizing = "border-box" 
+    // Box
+    // Image
+    let imageCSS = image.style
+    image.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FCE6zbBdaR9O8ixpQ16t6&w=1920&q=75"
+    imageCSS.height = "72%"
+    imageCSS.width = " 87%"
+    imageCSS.margin = "20px 0px 0px 20px"
+    // Image
+    // Word
+    word.innerText = "Unisex Long Sleeve Tee"
+    let wordCSS = word.style
+    wordCSS.fontSize = "1.125rem"
+    wordCSS.fontWeight = "600"
+    wordCSS.color = "rgba(31,41,55,var(--tw-text-opacity))"
+    wordCSS.display = "flex"
+    wordCSS.justifyContent = "center"
+    wordCSS.fontFamily = "system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji"
+    wordCSS.margin = "0px 0px 10px 0px"
+    // Word
+    // Price
+    price.innerText = "£1.00"
+    let priceCSS = price.style
+    priceCSS.fontFamily = "f system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji"
+    priceCSS.fontSize = ".875rem"
+    priceCSS.display = "flex"
+    priceCSS.justifyContent = "center"
+    priceCSS.color = "grey"
+    priceCSS.padding = "0px 0px 20px 0px"
+    // Price
 }
+
+
