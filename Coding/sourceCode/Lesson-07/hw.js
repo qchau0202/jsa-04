04866b22bf1aea35db7d9a4689a1040adeb2a59c
quocchau0202 <quocchau4729@gmail.com>
2021-10-30 09:06:20 +0700

    Lesson-07-Homework

--- Coding/sourceCode/Lesson-07/hw.js
+++ Coding/sourceCode/Lesson-07/hw.js
@@ -1,69 +1,44 @@
-// let inputEl = document.createElement("input");
-// inputEl.type = "text";
-// inputEl.className = "myInput text-white font-bold";
-
-// parentEl.appendChild(inputEl);
-
 const parentEl = document.getElementById("parent");
-let parentCSS = parentEl.style
-// parentCSS.border = "solid";
-parentCSS.fontSize = "200px";
-parentCSS.width = "100%"
-parentCSS.height = "1000px"
-parentCSS.padding = "0px 225px 0px 225px"
-parentCSS.display = "flex"
-parentCSS.justifyContent ="space-between"
-parentCSS.flexWrap = "wrap"
 for (let i = 0; i < 7; i++) {
-    // Box
     let product = document.createElement("div");
-    parentEl.appendChild(product);
-    product.className = "product"
-    let image = document.createElement("img")
-    product.appendChild(image)
-    let word  = document.createElement("p")
-    product.appendChild(word);
-    let price = document.createElement("p")
-    product.appendChild(price)
-    let productCSS = product.style
-    productCSS.width = "32%"
-    // productCSS.padding = "24px"
-    productCSS.height = "450px"
-    // productCSS.border = "1px solid"
-    productCSS.margin = "0px 0px 40px 0px"
-    productCSS.padding = "0px 0px 20px 0px"
-    productCSS.backgroundColor = "rgba(249,250,251)"
-    productCSS.borderRadius="0.5rem"
-    productCSS.boxSizing = "border-box" 
-    // Box
-    // Image
-    let imageCSS = image.style
-    image.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FCE6zbBdaR9O8ixpQ16t6&w=1920&q=75"
-    imageCSS.height = "72%"
-    imageCSS.width = " 87%"
-    imageCSS.margin = "20px 0px 0px 20px"
-    // Image
-    // Word
-    word.innerText = "Unisex Long Sleeve Tee"
-    let wordCSS = word.style
-    wordCSS.fontSize = "1.125rem"
-    wordCSS.fontWeight = "600"
-    wordCSS.color = "rgba(31,41,55,var(--tw-text-opacity))"
-    wordCSS.display = "flex"
-    wordCSS.justifyContent = "center"
-    wordCSS.fontFamily = "system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji"
-    wordCSS.margin = "0px 0px 10px 0px"
-    // Word
-    // Price
-    price.innerText = "£1.00"
-    let priceCSS = price.style
-    priceCSS.fontFamily = "f system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji"
-    priceCSS.fontSize = ".875rem"
-    priceCSS.display = "flex"
-    priceCSS.justifyContent = "center"
-    priceCSS.color = "grey"
-    priceCSS.padding = "0px 0px 20px 0px"
-    // Price
+    product.id = "productMain"
+    parentEl.appendChild(product)
+//Product
+let pr1 = document.createElement("article")
+//Img
+let image1 = document.createElement("img")
+image1.className = "img"
+image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FCE6zbBdaR9O8ixpQ16t6&w=1920&q=75"
+pr1.appendChild(image1)
+//Text
+let text1 = document.createElement("p")
+text1.className = "text"
+text1.innerText = "Unisex Long Sleeve Tee"
+pr1.appendChild(text1);
+//Price
+let price1 = document.createElement("p")
+price1.className = "price"
+price1.innerText = "£1.00"
+pr1.appendChild(price1);
+product.appendChild(pr1);
+if(i == 1){
+image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FzWiDwpcQ8msYs0JYm7o2&w=1920&q=75"
+text1.innerText = "Snapback"
+} else if (i == 2){
+    image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FG0GMUFSPQoyzuYjiRdjw&w=1920&q=75"
+    text1.innerText = "Unisex Zip hoodie"
+} else if (i==3){
+image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2F9PMP8m9KSTuL6yH0loET&w=1920&q=75"
+text1.innerText = "Mug"
+} else if (i ==4){
+image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FosJ4arNrTNykmUji9qIK&w=1920&q=75"
+text1.innerText = "Short Sleeve Tee"
+} else if (i==5){
+    image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2FuXRQVf3wT2OvJBP3c7r0&w=1920&q=75"
+    text1.innerText = "Tote Bag"
+} else if (i == 6){
+    image1.src = "https://commerce.withheadlesscms.com/_next/image?url=https%3A%2F%2Fmedia.graphcms.com%2F3qnaWF0SqieJL9tpWUxq&w=1920&q=75"
+    text1.innerText = "Backpack"
 }
-
+}
 
